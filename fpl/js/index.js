1a52f4eb190c39c4de3dab87a964cd857e1f9256
thanhbui <buihuuthanh95@gmail.com>
2021-09-09 22:00:13 +0700

    fix undefined transfer cost

--- fpl/js/index.js
+++ fpl/js/index.js
@@ -1,17 +1,25 @@
 const LEAGUE_CLASSIC_ID = '290179'
 
-const getInfo = async (url, params, callback) => {
+const getInfo = async (url, params, callback, errorCallBack) => {
     try {
         const result = await fetch(url, {
             method: "GET",
-            headers: {"Content-type": "application/json;charset=UTF-8"}
-          })
-          .then(response => response.json())
-          .then(json => callback(json, params))
+            headers: {
+                "Content-type": "application/json;charset=UTF-8",
+                "Access-Control-Allow-Origin": "*"
+            }
+        })
+            .then(response => response.json())
+            .then(json => callback(json, params))
     } catch (err) {
         console.log(err)
+        errorCallBack()
     }
 }
+
+const spinner = document.querySelector('.js-spinner')
+const spinnerLoadingText = document.querySelector('.js-spinner-loading-text')
+const tableListMember = document.querySelector('.js-table-list-member')
 
 let gw = 1
 
@@ -72,92 +80,118 @@ const resetTableInfo = () => {
 
 
 const getListClassicMemberId = (leagueId, gameweek) => {
-    const BASE_URL = 'https://fantasy.premierleague.com/api/leagues-classic/'
+    gameWeekSelect.disabled = true
+    spinner.style.display = 'block'
+    tableListMember.style.display = 'none'
 
-    const listMember = []
+    const listMemberCache = localStorage.getItem(gw.toString())
 
-    const listPromise = []
-
-    const processData = (data, params) => {
-        const { page, pageType } = params
-
-        const standings = data ? data.standings : null
-
-        if (standings) {
-            if (standings.results) {
-                standings.results.forEach(element => {
-                    listMember.push(element)
-                });
-            }
-        } else {
-            console.log('Data is null')
-        }
-    }
-
-    const getListMemberPage = async (page, pageType = '1') => {
-        const url = `${BASE_URL}${leagueId}/standings/?page_new_entries=1&page_standings=${page}&phase=${pageType}`
-
-        const params = {
-            page,
-            pageType,
-        }
-
-        await getInfo(url, params, processData)
-    }
-
-    const promise1 = getListMemberPage('1')
-    const promise2 = getListMemberPage('2')
-    listPromise.push(promise1)
-    listPromise.push(promise2)
-    
-
-    Promise.all(listPromise).then(() => {
-        listMember.forEach(async (member, index) => {
-            const entryId = member.entry
-
-            const urlInfo = `https://fantasy.premierleague.com/api/entry/${entryId}/event/${gameweek}/picks/`
-
-            await getInfo(urlInfo, null, (data) => {
-                const newMember = {
-                    ...member,
-                }
-                const { points, event_transfers: transfers, event_transfers_cost: transfersCost } = data.entry_history
-                newMember.event_total = points
-                newMember.transfers = transfers
-                newMember.transfersCost = transfersCost
-                newMember.total = newMember.event_total - transfersCost
-                if (transfersCost > 0) console.log(member, transfersCost)
-
-                listMember.splice(index, 1, newMember)
-
-                if (index === listMember.length - 1) {
-                    console.log(listMember)
-
-                    const rank = listMember.sort((m1, m2) => {
-                        if (m1.total < m2.total) return 1
-                        else if (m1.total === m2.total) return 0
-                        return -1
-                    })
-
-                    console.log(rank)
-
-                    rank.forEach((m) => {
-                        createTableRow(m)
-                    })
-                }
-            })
+    if (listMemberCache !== null) {
+        const rank = JSON.parse(listMemberCache).sort((m1, m2) => {
+            if (m1.total < m2.total) return 1
+            else if (m1.total === m2.total) return 0
+            return -1
         })
 
+
+        spinner.style.display = 'none'
+        tableListMember.style.display = 'table'
+
+        rank.forEach((m) => {
+            createTableRow(m)
+        })
+
+        gameWeekSelect.disabled = false
         
-        
-        // console.log(rank)
-    })
-    return listMember
+        return listMemberCache
+    } else {
+        const BASE_URL = 'https://fantasy.premierleague.com/api/leagues-classic/'
+
+        const listMember = []
+
+        const listPromise = []
+
+        const processData = (data, params) => {
+            const { page, pageType } = params
+
+            const standings = data ? data.standings : null
+
+            if (standings) {
+                if (standings.results) {
+                    standings.results.forEach(element => {
+                        listMember.push(element)
+                    });
+                }
+            } else {
+                console.log('Data is null')
+            }
+        }
+
+        const getListMemberPage = async (page, pageType = '1') => {
+            const url = `${BASE_URL}${leagueId}/standings/?page_new_entries=1&page_standings=${page}&phase=${pageType}`
+
+            const params = {
+                page,
+                pageType,
+            }
+
+            await getInfo(url, params, processData)
+        }
+
+        const promise1 = getListMemberPage('1')
+        const promise2 = getListMemberPage('2')
+        listPromise.push(promise1)
+        listPromise.push(promise2)
+
+
+        Promise.all(listPromise).then(async () => {
+            const length = listMember.length
+
+            let index = 0
+            for (member of listMember) {
+                const entryId = member.entry
+
+                const urlInfo = `https://fantasy.premierleague.com/api/entry/${entryId}/event/${gameweek}/picks/`
+
+                await getInfo(urlInfo, null, (data) => {
+                    const newMember = {
+                        ...member,
+                    }
+                    const { points, event_transfers: transfers, event_transfers_cost: transfersCost } = data.entry_history
+                    newMember.event_total = points
+                    newMember.transfers = transfers
+                    newMember.transfersCost = transfersCost || 0
+                    newMember.total = newMember.event_total - transfersCost
+                    if (transfersCost > 0) console.log(member, transfersCost)
+
+                    listMember.splice(index, 1, newMember)
+                })
+
+                index += 1
+
+                spinnerLoadingText.innerHTML = `${Math.floor(index / length * 100)}%`
+            }
+
+            const rank = listMember.sort((m1, m2) => {
+                if (m1.total < m2.total) return 1
+                else if (m1.total === m2.total) return 0
+                return -1
+            })
+
+            localStorage.setItem(gw.toString(), JSON.stringify(listMember))
+
+            spinner.style.display = 'none'
+            tableListMember.style.display = 'table'
+
+            rank.forEach((m) => {
+                createTableRow(m)
+            })
+
+            gameWeekSelect.disabled = false
+        })
+        return listMember
+    }
 }
-
-// getInfo('290179/standings/?page_new_entries=1&page_standings=1&phase=1', null,  )
-getInfo('https://fantasy.premierleague.com/api/entry/375/event/2/picks/', null, data => console.log(data))
-
 
 const reloadPage = () => {
     resetTableInfo()
