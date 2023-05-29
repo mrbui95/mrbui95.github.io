7077964bd3d5ff036e2c3c26e1d363f3202420ab
Thanh Bui <buihuuthanh95@gmail.com>
2023-05-30 05:53:53 +0700

    update current gw

--- amvn/js/gw_dropdown.js
+++ amvn/js/gw_dropdown.js
@@ -1,24 +1,43 @@
 const gameWeekSelect = document.querySelector('#gw_select')
 
 
-currentGw = 1
+let currentGw = -1
 
 const getCurrentGW =  async () => {
-    fetch("https://mrbui95.github.io/fpl/data/current_gw.json", {
+    await fetch("https://mrbui95.github.io/fpl/data/current_gw.json", {
         method: "GET",
         headers: {
             "Content-type": "application/json;charset=UTF-8",
             "Access-Control-Allow-Origin": "*"
         }
-    }).then(data => console.log(data.json()))
+    }).then(data => {
+        return data.json()
+    }).then((json) => {
+        let id
+        let gameWeekSelectOption
+
+        if (currentGw != -1) {
+            id = 'dropdown_gw_option_' + currentGw;
+            gameWeekSelectOption = document.getElementById(id)
+            gameWeekSelectOption.removeAttribute('selected')
+        }
+
+        currentGw = json.current_gw
+        console.log(json, currentGw)
+        id = 'dropdown_gw_option_' + currentGw;
+        gameWeekSelectOption = document.getElementById(id)
+        gameWeekSelectOption.setAttribute('selected', true)
+    })
 }
 
+getCurrentGW()
 
 
 
 for (let i = 1; i < 39; i += 1) {
     const option = document.createElement('option')
     option.appendChild(document.createTextNode(i))
+    option.setAttribute('id', 'dropdown_gw_option_' + i)
     if (i === currentGw) {
         option.setAttribute('selected', true)
     }
@@ -35,7 +54,4 @@ gameWeekSelect.onchange = gameWeekSelectOnChange
 
 const reloadPage = () => {
     console.log('reload page')
-    getCurrentGW()
 }
-
-getCurrentGW()
