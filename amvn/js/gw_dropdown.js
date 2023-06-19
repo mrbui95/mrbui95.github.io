3d3b55fa932fe460a55b64aed2b01f14c6288c94
Thanh Bui <buihuuthanh95@gmail.com>
2023-06-20 04:39:32 +0700

    add gw rank2, rank ui

--- amvn/js/gw_dropdown.js
+++ amvn/js/gw_dropdown.js
@@ -4,9 +4,10 @@ const gameWeekSelect = document.querySelector('#gw_select')
 let currentGw = -1
 let uInfo
 let gwData
+let gwRank
 
 
-const getCurrentGW =  async () => {
+const getCurrentGW = async () => {
     await fetch("https://mrbui95.github.io/fpl/data/current_gw.json", {
         method: "GET",
         headers: {
@@ -110,8 +111,76 @@ const createGroupResult = (groupInfo) => {
     })
 
     table.appendChild(tbody)
-
     result.appendChild(table)
+
+    result.appendChild(document.createTextNode('BXH'))
+
+    const table2 = document.createElement('table')
+    table2.classList.add('table')
+    table2.classList.add('table-bordered')
+    table2.classList.add('table-striped')
+    table2.classList.add('table-sm')
+    const tbody2 = document.createElement('tbody')
+
+    const head2 = document.createElement('thead')
+    const trHead2 = document.createElement('tr')
+    
+    const tdHead2_1 = document.createElement('td')
+    const tdHead2_2 = document.createElement('td')
+    const tdHead2_3 = document.createElement('td')
+    const tdHead2_4 = document.createElement('td')
+    const tdHead2_5 = document.createElement('td')
+    const tdHead2_6 = document.createElement('td')
+
+    tdHead2_1.appendChild(document.createTextNode('HLV'))
+    tdHead2_2.appendChild(document.createTextNode('T'))
+    tdHead2_3.appendChild(document.createTextNode('H'))
+    tdHead2_4.appendChild(document.createTextNode('B'))
+    tdHead2_5.appendChild(document.createTextNode('HS'))
+    tdHead2_6.appendChild(document.createTextNode('Đ'))
+
+    trHead2.appendChild(tdHead2_1)
+    trHead2.appendChild(tdHead2_2)
+    trHead2.appendChild(tdHead2_3)
+    trHead2.appendChild(tdHead2_4)
+    trHead2.appendChild(tdHead2_5)
+    trHead2.appendChild(tdHead2_6)
+
+    head2.append(trHead2)
+    table2.appendChild(head2)
+
+    groupInfo.rank.forEach((rank) => {
+        const tr2 = document.createElement('tr')
+        const td2_1 = document.createElement('td')
+        const td2_2 = document.createElement('td')
+        const td2_3 = document.createElement('td')
+        const td2_4 = document.createElement('td')
+        const td2_5 = document.createElement('td')
+        const td2_6 = document.createElement('td')
+
+        td2_1.appendChild(document.createTextNode(rank.name))
+        td2_2.appendChild(document.createTextNode(rank.win))
+        td2_3.appendChild(document.createTextNode(rank.draw))
+        td2_4.appendChild(document.createTextNode(rank.lose))
+        td2_5.appendChild(document.createTextNode(rank.gd))
+        td2_6.appendChild(document.createTextNode(rank.point))
+
+        tr2.appendChild(td2_1)
+        tr2.appendChild(td2_2)
+        tr2.appendChild(td2_3)
+        tr2.appendChild(td2_4)
+        tr2.appendChild(td2_5)
+        tr2.appendChild(td2_6)
+
+        tbody2.appendChild(tr2)
+    })
+
+    table2.appendChild(tbody2)
+    result.appendChild(table2)
+
+
+    result.appendChild(document.createTextNode('---------------------------------------------------'))
+
 
     // return result
     groupResults.appendChild(result)
@@ -132,6 +201,38 @@ const getUserData = async () => {
 
     let gw = currentGw % 19
     if (gw < 8) {
+        await fetch("https://mrbui95.github.io/fpl/data/c1/rank/" + gw + ".json", {
+            method: "GET",
+            headers: {
+                "Content-type": "application/json;charset=UTF-8",
+                "Access-Control-Allow-Origin": "*"
+            }
+        }).then(data => {
+            return data.json()
+        }).then((data) => {
+            console.log(data)
+            let rank = []
+            for (let i = 1; i <= 8; i++) {
+                const groupInfo = data[i]
+                const groupPointRs = Object.keys(groupInfo).map(key => {
+                    const rs = groupInfo[key]
+                    rs.uid = key
+                    rs.name = uInfo[key]['name']
+                    return rs
+                });
+                groupPointRs.sort((u1, u2) => {
+                    if (u1.point == u2.point) {
+                        return u1.gd < u2.gd ? 1 : -1
+                    }
+                    return u1.point < u2.point ? 1 : -1
+                })
+                console.log(groupPointRs)
+                rank[i] = groupPointRs
+            }
+            gwRank = rank
+        })
+
+
         await fetch("https://mrbui95.github.io/fpl/data/c1/result/" + gw + ".json", {
             method: "GET",
             headers: {
@@ -161,7 +262,7 @@ const getUserData = async () => {
 
                 let listMatchResult = []
 
-                console.log('-----------', groupName ,'----------')
+                console.log('-----------', groupName, '----------')
                 fixture.forEach((match) => {
                     console.log(match)
                     const team1 = match[0]
@@ -184,29 +285,34 @@ const getUserData = async () => {
 
                 })
 
+                console.log(gwRank[i])
+
                 groupInfo = {
                     listMatchResult,
-                    groupName
+                    groupName,
+                    rank: gwRank[i]
                 }
 
                 result = createGroupResult(groupInfo)
-                
 
-                console.log('-----------', groupName ,'----------')
 
-                
-                
+                console.log('-----------', groupName, '----------')
+
+
+
             }
         })
+
     } else {
         console.log(gw, 'GW > 8')
     }
 
 
-    
+
 }
 
 getUserData()
+
 
 const gameWeekSelectOnChange = (e) => {
     currentGw = e.target.value
