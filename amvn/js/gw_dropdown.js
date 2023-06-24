96b5ce842cde772f6b1810c52a07f46fc8d234c2
Thanh Bui <buihuuthanh95@gmail.com>
2023-06-24 11:57:41 +0700

    fix bug stage 1

--- amvn/js/gw_dropdown.js
+++ amvn/js/gw_dropdown.js
@@ -86,7 +86,7 @@ const getGroupName = (index, groupType) => {
     }
 
     return 'Group'
-    
+
 }
 
 const groupResults = document.querySelector('#group_result')
@@ -96,7 +96,7 @@ const clearGroupResult = () => {
     while (groupResults.firstChild) groupResults.removeChild(groupResults.firstChild);
 }
 
-const createGroupResult = (groupInfo) => {
+const createGroupResult = (groupType, groupInfo) => {
     const result = document.createElement('div')
 
 
@@ -109,145 +109,213 @@ const createGroupResult = (groupInfo) => {
     result.appendChild(groupNameEle)
 
 
+    if (groupType === 'PHAN_HANG') {
+        const table = document.createElement('table')
+        table.classList.add('table')
+        table.classList.add('table-bordered')
+        table.classList.add('table-striped')
+        table.classList.add('table-sm')
+        const tbody = document.createElement('tbody')
 
 
-    // Kết quả trận đấu vòng
-    const table = document.createElement('table')
-    table.classList.add('table')
-    table.classList.add('table-bordered')
-    table.classList.add('table-striped')
-    table.classList.add('table-sm')
-    const tbody = document.createElement('tbody')
+        const head = document.createElement('thead')
+        const trHead = document.createElement('tr')
 
-    groupInfo.listMatchResult.forEach((match) => {
+        const tdHead_1 = document.createElement('td')
+        const tdHead_2 = document.createElement('td')
+        const tdHead_3 = document.createElement('td')
+        const tdHead_4 = document.createElement('td')
+        const tdHead_5 = document.createElement('td')
+        const tdHead_6 = document.createElement('td')
+        const tdHead_7 = document.createElement('td')
 
-        const tr = document.createElement('tr')
-        const td1 = document.createElement('td')
-        const td2 = document.createElement('td')
-        const td3 = document.createElement('td')
-        const td4 = document.createElement('td')
+        tdHead_1.appendChild(document.createTextNode('Hạng'))
+        tdHead_2.appendChild(document.createTextNode('HLV'))
+        tdHead_3.appendChild(document.createTextNode('GW'))
+        tdHead_4.appendChild(document.createTextNode('TT'))
+        tdHead_5.appendChild(document.createTextNode('TV'))
 
-        
-        if (match.point1 > match.point2) {
-            td1.classList.add('winner_cell')
-        } else if (match.point1 < match.point2) {
-            td4.classList.add('winner_cell')
-        }
+        trHead.appendChild(tdHead_1)
+        trHead.appendChild(tdHead_2)
+        trHead.appendChild(tdHead_3)
+        trHead.appendChild(tdHead_4)
+        trHead.appendChild(tdHead_5)
 
-        td1.appendChild(document.createTextNode(match.name1))
-        td2.appendChild(document.createTextNode(match.point1))
-        td3.appendChild(document.createTextNode(match.point2))
-        td4.appendChild(document.createTextNode(match.name2))
+        head.append(trHead)
+        table.appendChild(head)
 
-        tr.appendChild(td1)
-        tr.appendChild(td2)
-        tr.appendChild(td3)
-        tr.appendChild(td4)
+        let index = 1;
 
-        tbody.appendChild(tr)
-    })
+        groupInfo.rank.forEach((rank) => {
+            const tr = document.createElement('tr')
+            const td_1 = document.createElement('td')
+            const td_2 = document.createElement('td')
+            const td_3 = document.createElement('td')
+            const td_4 = document.createElement('td')
+            const td_5 = document.createElement('td')
 
-    table.appendChild(tbody)
-    table.style.border = 'solid 1px'
+            td_1.appendChild(document.createTextNode(index))
+            td_2.appendChild(document.createTextNode(rank.name))
+            td_3.appendChild(document.createTextNode(rank.gw_point))
+            td_4.appendChild(document.createTextNode(rank.total_point))
+            td_5.appendChild(document.createTextNode(rank.team_cost))
 
-    result.appendChild(table)
+            tr.appendChild(td_1)
+            tr.appendChild(td_2)
+            tr.appendChild(td_3)
+            tr.appendChild(td_4)
+            tr.appendChild(td_5)
 
-    result.appendChild(document.createTextNode('BXH'))
+            switch (index) {
+                case 1:
+                case 2:
+                    tr.classList.add('rank_c1')
+                    break
+                case 3:
+                case 4:
+                    tr.classList.add('rank_c2')
+                    break
+                case 5:
+                case 6:
+                    tr.classList.add('rank_c3')
+                    break
+                case 7:
+                case 8:
+                    tr.classList.add('rank_c4')
+                    break
+            }
 
-    const table2 = document.createElement('table')
-    table2.classList.add('table')
-    table2.classList.add('table-bordered')
-    table2.classList.add('table-striped')
-    table2.classList.add('table-sm')
-    const tbody2 = document.createElement('tbody')
+            tbody.appendChild(tr)
+            index += 1
+        })
 
-    const head2 = document.createElement('thead')
-    const trHead2 = document.createElement('tr')
+    } else if (groupType === 'C_1234') {
+        // Kết quả trận đấu vòng
+        const table = document.createElement('table')
+        table.classList.add('table')
+        table.classList.add('table-bordered')
+        table.classList.add('table-striped')
+        table.classList.add('table-sm')
+        const tbody = document.createElement('tbody')
 
-    const tdHead2_1 = document.createElement('td')
-    const tdHead2_2 = document.createElement('td')
-    const tdHead2_3 = document.createElement('td')
-    const tdHead2_4 = document.createElement('td')
-    const tdHead2_5 = document.createElement('td')
-    const tdHead2_6 = document.createElement('td')
-    const tdHead2_7 = document.createElement('td')
+        groupInfo.listMatchResult.forEach((match) => {
 
-    tdHead2_1.appendChild(document.createTextNode('Hạng'))
-    tdHead2_2.appendChild(document.createTextNode('HLV'))
-    tdHead2_3.appendChild(document.createTextNode('T'))
-    tdHead2_4.appendChild(document.createTextNode('H'))
-    tdHead2_5.appendChild(document.createTextNode('B'))
-    tdHead2_6.appendChild(document.createTextNode('HS'))
-    tdHead2_7.appendChild(document.createTextNode('Đ'))
-
-    trHead2.appendChild(tdHead2_1)
-    trHead2.appendChild(tdHead2_2)
-    trHead2.appendChild(tdHead2_3)
-    trHead2.appendChild(tdHead2_4)
-    trHead2.appendChild(tdHead2_5)
-    trHead2.appendChild(tdHead2_6)
-    trHead2.appendChild(tdHead2_7)
-
-    head2.append(trHead2)
-    table2.appendChild(head2)
-
-    let index = 1;
-
-    groupInfo.rank.forEach((rank) => {
-        const tr2 = document.createElement('tr')
-        const td2_1 = document.createElement('td')
-        const td2_2 = document.createElement('td')
-        const td2_3 = document.createElement('td')
-        const td2_4 = document.createElement('td')
-        const td2_5 = document.createElement('td')
-        const td2_6 = document.createElement('td')
-        const td2_7 = document.createElement('td')
-
-        td2_1.appendChild(document.createTextNode(index))
-        td2_2.appendChild(document.createTextNode(rank.name))
-        td2_3.appendChild(document.createTextNode(rank.win))
-        td2_4.appendChild(document.createTextNode(rank.draw))
-        td2_5.appendChild(document.createTextNode(rank.lose))
-        td2_6.appendChild(document.createTextNode(rank.gd))
-        td2_7.appendChild(document.createTextNode(rank.point))
-
-        tr2.appendChild(td2_1)
-        tr2.appendChild(td2_2)
-        tr2.appendChild(td2_3)
-        tr2.appendChild(td2_4)
-        tr2.appendChild(td2_5)
-        tr2.appendChild(td2_6)
-        tr2.appendChild(td2_7)
-
-        switch (index) {
-            case 1:
-            case 2:
-                tr2.classList.add('rank_c1')
-                break
-            case 3:
-            case 4:
-                tr2.classList.add('rank_c2')
-                break
-            case 5:
-            case 6:
-                tr2.classList.add('rank_c3')
-                break
-            case 7:
-            case 8:
-                tr2.classList.add('rank_c4')
-                break
-        }
-
-        tbody2.appendChild(tr2)
-        index += 1
-    })
-
-    table2.appendChild(tbody2)
-    table2.style.border = 'solid 1px'
-    result.appendChild(table2)
+            const tr = document.createElement('tr')
+            const td1 = document.createElement('td')
+            const td2 = document.createElement('td')
+            const td3 = document.createElement('td')
+            const td4 = document.createElement('td')
 
 
-    result.appendChild(document.createTextNode('---------------------------------------------------'))
+            if (match.point1 > match.point2) {
+                td1.classList.add('winner_cell')
+            } else if (match.point1 < match.point2) {
+                td4.classList.add('winner_cell')
+            }
+
+            td1.appendChild(document.createTextNode(match.name1))
+            td2.appendChild(document.createTextNode(match.point1))
+            td3.appendChild(document.createTextNode(match.point2))
+            td4.appendChild(document.createTextNode(match.name2))
+
+            tr.appendChild(td1)
+            tr.appendChild(td2)
+            tr.appendChild(td3)
+            tr.appendChild(td4)
+
+            tbody.appendChild(tr)
+        })
+
+        table.appendChild(tbody)
+        table.style.border = 'solid 1px'
+
+        result.appendChild(table)
+
+        result.appendChild(document.createTextNode('BXH'))
+
+        const table2 = document.createElement('table')
+        table2.classList.add('table')
+        table2.classList.add('table-bordered')
+        table2.classList.add('table-striped')
+        table2.classList.add('table-sm')
+        const tbody2 = document.createElement('tbody')
+
+        const head2 = document.createElement('thead')
+        const trHead2 = document.createElement('tr')
+
+        const tdHead2_1 = document.createElement('td')
+        const tdHead2_2 = document.createElement('td')
+        const tdHead2_3 = document.createElement('td')
+        const tdHead2_4 = document.createElement('td')
+        const tdHead2_5 = document.createElement('td')
+        const tdHead2_6 = document.createElement('td')
+        const tdHead2_7 = document.createElement('td')
+
+        tdHead2_1.appendChild(document.createTextNode('Hạng'))
+        tdHead2_2.appendChild(document.createTextNode('HLV'))
+        tdHead2_3.appendChild(document.createTextNode('T'))
+        tdHead2_4.appendChild(document.createTextNode('H'))
+        tdHead2_5.appendChild(document.createTextNode('B'))
+        tdHead2_6.appendChild(document.createTextNode('HS'))
+        tdHead2_7.appendChild(document.createTextNode('Đ'))
+
+        trHead2.appendChild(tdHead2_1)
+        trHead2.appendChild(tdHead2_2)
+        trHead2.appendChild(tdHead2_3)
+        trHead2.appendChild(tdHead2_4)
+        trHead2.appendChild(tdHead2_5)
+        trHead2.appendChild(tdHead2_6)
+        trHead2.appendChild(tdHead2_7)
+
+        head2.append(trHead2)
+        table2.appendChild(head2)
+
+        let index = 1;
+
+        groupInfo.rank.forEach((rank) => {
+            const tr2 = document.createElement('tr')
+            const td2_1 = document.createElement('td')
+            const td2_2 = document.createElement('td')
+            const td2_3 = document.createElement('td')
+            const td2_4 = document.createElement('td')
+            const td2_5 = document.createElement('td')
+            const td2_6 = document.createElement('td')
+            const td2_7 = document.createElement('td')
+
+            td2_1.appendChild(document.createTextNode(index))
+            td2_2.appendChild(document.createTextNode(rank.name))
+            td2_3.appendChild(document.createTextNode(rank.win))
+            td2_4.appendChild(document.createTextNode(rank.draw))
+            td2_5.appendChild(document.createTextNode(rank.lose))
+            td2_6.appendChild(document.createTextNode(rank.gd))
+            td2_7.appendChild(document.createTextNode(rank.point))
+
+            tr2.appendChild(td2_1)
+            tr2.appendChild(td2_2)
+            tr2.appendChild(td2_3)
+            tr2.appendChild(td2_4)
+            tr2.appendChild(td2_5)
+            tr2.appendChild(td2_6)
+            tr2.appendChild(td2_7)
+
+            if (index <= 8) {
+                tr2.classList.add('winner_cell')
+            }
+
+            tbody2.appendChild(tr2)
+            index += 1
+        })
+
+        table2.appendChild(tbody2)
+        table2.style.border = 'solid 1px'
+        result.appendChild(table2)
+
+        result.appendChild(document.createTextNode('---------------------------------------------------'))
+
+    }
+
+
+
 
 
     // return result
@@ -281,7 +349,7 @@ const getUserData = async () => {
         urlGroupFixture = 'https://mrbui95.github.io/fpl/data/c1/group_period2_1.json'
         maxGroup = 4
         groupType = 'C_1234'
-        fixtureIndex = gw -8
+        fixtureIndex = gw - 8
     }
 
 
@@ -316,7 +384,7 @@ const getUserData = async () => {
             });
             groupPointRs.sort((u1, u2) => {
                 if (u1.point == u2.point) {
-                    return u1.gd < u2.gd ? 1 : -1 
+                    return u1.gd < u2.gd ? 1 : -1
                 }
                 return u1.point < u2.point ? 1 : -1
             })
@@ -327,23 +395,36 @@ const getUserData = async () => {
     })
 
     await fetch(urlGroupFixture, {
-            method: "GET",
-            headers: {
-                "Content-type": "application/json;charset=UTF-8",
-                "Access-Control-Allow-Origin": "*"
-            }
-        }).then(data => {
-            return data.json()
-        }).then((groupData) => {
-            console.log(groupData)
+        method: "GET",
+        headers: {
+            "Content-type": "application/json;charset=UTF-8",
+            "Access-Control-Allow-Origin": "*"
+        }
+    }).then(data => {
+        return data.json()
+    }).then((groupData) => {
+        console.log(groupData)
 
-            clearGroupResult()
+        clearGroupResult()
 
-            for (let i = 1; i <= maxGroup; i++) {
+        for (let i = 1; i <= maxGroup; i++) {
+            const groupName = getGroupName(i, groupType)
+
+            if (gw <= 8) {
+                console.log(gwRank[i])
+
+                groupInfo = {
+                    index: i,
+                    groupName,
+                    rank: gwRank[i]
+                }
+
+                result = createGroupResult(groupType, groupInfo)
+            } else if (gw < 17) {
                 const fixture = groupData[i][fixtureIndex]
                 console.log(fixture)
 
-                const groupName = getGroupName(i, groupType)
+
                 listMatch = []
 
                 let listMatchResult = []
@@ -380,16 +461,13 @@ const getUserData = async () => {
                     rank: gwRank[i]
                 }
 
-                result = createGroupResult(groupInfo)
-
+                result = createGroupResult(groupType, groupInfo)
 
                 console.log('-----------', groupName, '----------')
-
-
-
             }
-        })
-    
+        }
+    })
+
 
 
 
