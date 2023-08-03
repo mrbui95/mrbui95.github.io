34ca76e622a5f0849e726be1bcc4c5e612f1d70e
Thanh Bui <buihuuthanh95@gmail.com>
2023-08-04 06:31:19 +0700

    init amvn23

--- amvn23/tools/getDataC1.py
+++ amvn23/tools/getDataC1.py
@@ -9,7 +9,7 @@ print(response.json())
 
 listTeam = response.json()['league']
 
-for currentGw in range (0, 1):
+for currentGw in range (1, 2):
     data={}
     headers = {'x-requested-with':'https://fantasy.premierleague.com', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36'}
 
