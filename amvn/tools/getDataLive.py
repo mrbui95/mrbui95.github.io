71af6ec052d52702419dcea7ca2c4e075a5cd2f8
ThanhBui <buihuuthanh95@gmail.com>
2024-01-30 12:40:20 +0700

    update

--- amvn/tools/getDataLive.py
+++ amvn/tools/getDataLive.py
@@ -16,7 +16,8 @@ data_curr_gw = response_curr_gw.json()
 current_gw = data_curr_gw['current_event']
 data = {}
 data['current_gw'] = current_gw
-fileName = 'F:\\Study\\Github\\mrbui95.github.io\\amvn\\data\\current_gw.json'
+#fileName = 'F:\\Study\\Github\\mrbui95.github.io\\amvn\\data\\current_gw.json'
+fileName = 'D:\\thanhbh3\\mrbui95.github.io\\amvn\\data\\current_gw.json'
 file = codecs.open(fileName, 'w', 'utf8')
 file.write(json.dumps(data))
 file.close()
@@ -27,7 +28,8 @@ print (current_gw, curr_gw_sv)
 if (current_gw != curr_gw_sv ):
     time.sleep(10)
     print('upload git')
-    repo_dir = 'F:\\Study\\Github\\mrbui95.github.io'
+    #repo_dir = 'F:\\Study\\Github\\mrbui95.github.io'
+    repo_dir = 'D:\\thanhbh3\\Github\\mrbui95.github.io'
     repo = Repo(repo_dir)
     file_list = [
         fileName
@@ -134,9 +136,9 @@ def get_current_point(uid):
 
 
 def get_gw19_point(gw19data, uid):
-    
-    print('===============GW19 result')
-    return udata[str(uid)]["point"]
+    print('===============GW19 result:' + json.dumps(gw19data[str(uid)]["entry_history"]))
+    return gw19data[str(uid)]["entry_history"]["total_points"]
+
 
 def job():
     print('============START==============')
@@ -159,12 +161,14 @@ def job():
         print('-------------' + uid + '----------------')
     print(json.dumps(gw_result))
 
-    fileName = 'F:\\Study\\Github\\mrbui95.github.io\\amvn\\data\\c1\\result\\' + str(current_gw) + '.json'
+    #fileName = 'F:\\Study\\Github\\mrbui95.github.io\\amvn\\data\\c1\\result\\' + str(current_gw) + '.json'
+    fileName = 'D:\\thanhbh3\\mrbui95.github.io\\amvn\\data\\c1\\result\\' + str(current_gw) + '.json'
     file = codecs.open(fileName, 'w', 'utf8')
     file.write(json.dumps(gw_result))
     file.close()
 
-    repo_dir = 'F:\\Study\\Github\\mrbui95.github.io'
+    #repo_dir = 'F:\\Study\\Github\\mrbui95.github.io'
+    repo_dir = 'D:\\thanhbh3\\mrbui95.github.io'
     repo = Repo(repo_dir)
     file_list = [
         fileName
@@ -217,12 +221,14 @@ def job():
     live_rank = get_rank()
     # print(live_rank)
 
-    fileName = 'F:\\Study\\Github\\mrbui95.github.io\\amvn\\data\\c1\\rank\\' + str(current_gw) + '.json'
+    #fileName = 'F:\\Study\\Github\\mrbui95.github.io\\amvn\\data\\c1\\rank\\' + str(current_gw) + '.json'
+    fileName = 'D:\\thanhbh3\\mrbui95.github.io\\amvn\\data\\c1\\rank\\' + str(current_gw) + '.json'
     file1 = codecs.open(fileName, 'w', 'utf8')
     file1.write(json.dumps(live_rank))
     file1.close()
 
-    repo_dir = 'F:\\Study\\Github\\mrbui95.github.io'
+    #repo_dir = 'F:\\Study\\Github\\mrbui95.github.io'
+    repo_dir = 'D:\\thanhbh3\\mrbui95.github.io'
     repo = Repo(repo_dir)
     file_list = [
         fileName
